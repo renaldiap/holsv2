4d0e086d1d9044662ae37240e8cb38f323c4575a
renaldiap <44296686+renaldiap@users.noreply.github.com>
2019-07-29 11:57:03 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,28 @@
+<?php
+
+//Maksimal 5 Akun 
+
+//Akun1
+$accountId1 = "xxxxxx";
+$accessToken1 = "xxxxxx";
+$username1 = "xxxxxx";
+
+//Akun2
+$accountId2 = "xxxxxx";
+$accessToken2 = "xxxxxx";
+$username2 = "xxxxx";
+
+//Akun3
+$accountId3 = "xxxxxx";
+$accessToken3 = "xxxxxx";
+$username3 = "xxxxx";
+
+//Akun4
+$accountId4 = "xxxxxx";
+$accessToken4 = "xxxxxx";
+$username4 = "xxxxx";
+
+//Akun5
+$accountId5 = "xxxxxx";
+$accessToken5 = "xxxxxx";
+$username5 = "xxxxx";
